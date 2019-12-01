e9a4ad9510359d569d10af9f67863c3cdeffc891
Oleg Bulatov <obulatov@mail.ru>
2019-12-01 21:57:36 +0400

    jqxGrid

---
+++ LAN_WORK/Controllers/DIOSController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanWork.Controllers
+{
+    public class DIOSController : Controller
+    {
+        //
+        // GET: /DIOS/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
