f4439ea409e6755038c0ad5e9f4e752bea7d1347
Oleg Bulatov <obulatov@mail.ru>
2020-03-28 11:19:30 +0400

    currentClass now lives in clientObj (cObj) clientObj in new class for encapsulate Obj logics (controllers etc)

---
+++ LAN_WORK/dist/vue_obj.js
@@ -0,0 +1,45 @@
+﻿(function ($) {
+    $.fn.clientObj = function (className) {
+        if (cObj)
+            cObj.destroy();
+
+        cObj = new Vue({
+            data: {
+                class_name: className,
+                controllers: []
+            },
+            methods: {
+                GetFilter: function () {
+                    return [];
+                },
+                GetDataAdapter: function () {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', '/Object/List?class_name=' + this.class_name + '&limit=0&offset=0', false);
+                    xhr.send();
+                    var data = JSON.parse(xhr.responseText);
+
+                    var source =
+                    {
+                        datatype: "array",
+                        //localdata: {},
+                        id: "id",
+                        updaterow: function (rowid, rowdata, commit) {
+                            // synchronize with the server - send update command
+                            // call commit with parameter true if the synchronization with the server is successful
+                            // and with parameter false if the synchronization failder.
+                            //commit(true);
+                        },
+                        totalrecords: data.row_count
+                    };
+
+                    var dataAdapter = new $.jqx.dataAdapter(source);
+                    return dataAdapter;
+                },
+                destroy: function () {
+                    this.$destroy();
+                }
+
+            }
+        });
+    };
+})(jQuery);
