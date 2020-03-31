94c247021ee732ea71ae4289f59ed312a020dfc2
Oleg Bulatov <obulatov@mail.ru>
2020-03-31 12:53:52 +0500

    init grid from cObj

--- LAN_WORK/dist/vue_obj.js
+++ LAN_WORK/dist/vue_obj.js
@@ -1,18 +1,256 @@
-﻿(function ($) {
+﻿
+(function ($) {
+    var cObj;
+    var editedObject = new Object();
+    var gridName = '#grid';
+    var SortState = new Object();
+    var LoadedState = new Object();
+    var ColumnModels = new Object();
+
+    function InitGridFromObj(className) {
+
+        function getColumnType(typeName) {
+            switch (typeName) {
+                case 'DIOS.Common.SqlBoolean':
+                    return 'checkbox';
+                case 'LongText':
+                    return 'template';
+                    return 'textbox';
+            }
+        }
+        var renderGridRows = function (params) {
+            var xhr = new XMLHttpRequest();
+            var sortColumn = cObj.class_name;
+            if (SortState[cObj.class_name]) {
+                sortColumn = SortState[cObj.class_name].sortcolumn;
+                if (SortState[cObj.class_name].sortdirection.descending)
+                    sortColumn += ' desc';
+            }
+            xhr.open('GET', '/Object/List?class_name=' + cObj.class_name + '&filter=' + cObj.GetFilter() + '&order=' + sortColumn + '&limit=10&offset=' + params.startindex, false);
+
+            xhr.send();
+            var data = JSON.parse(xhr.responseText);
+            return JSON.parse(data.response_body);
+        }
+        var renderToolBar = function (statusbar) {
+            // appends buttons to the status bar.
+            var container = $("<div style='overflow: hidden; position: relative; margin: 5px;'></div>");
+            var tuneButton = $("<div style='float: left; margin-left: 5px;'><img style='position: relative; margin-top: 2px;' src='/images/property.gif'/><span style='margin-left: 4px; position: relative; top: -3px;'>Tune</span></div>");
+            var reloadButton = $("<div style='float: left; margin-left: 5px;'><img style='position: relative; margin-top: 2px;' src='/images/refresh.gif'/><span style='margin-left: 4px; position: relative; top: -3px;'>Reload</span></div>");
+            var saveStateButton = $("<div style='float: left; margin-left: 5px;'><img style='position: relative; margin-top: 2px;' src='/images/save.gif'/><span style='margin-left: 4px; position: relative; top: -3px;'>Save</span></div>");
+            //                        var loadStateButton = $("<div style='float: left; margin-left: 5px;'><img style='position: relative; margin-top: 2px;' src='/images/refresh.gif'/><span style='margin-left: 4px; position: relative; top: -3px;'>Load</span></div>");
+            container.append(tuneButton);
+            container.append(reloadButton);
+            container.append(saveStateButton);
+            statusbar.append(container);
+            tuneButton.jqxButton({ theme: theme });
+            reloadButton.jqxButton({ theme: theme });
+            saveStateButton.jqxButton({ theme: theme });
+            //                        loadStateButton.jqxButton({ theme: theme });
+
+            var isTuning;
+            var tuneDiv;
+            //var state = null;
+            tuneButton.click(function (event) {
+                if (isTuning) {
+                    isTuning = false;
+                    tuneDiv.hide();
+                    return;
+                }
+                isTuning = true;
+                tuneDiv = $("<div>");
+                tuneDiv.css({
+                    "position": "absolute",
+                    "top": statusbar.offset().top + 40,
+                    "left": statusbar.offset().left,
+                    "z-index": "35",
+                    "background-color": "white",
+                    "border": "2px solid blue"
+                });
+                //var okButton = $("<div style='position: absolute; margin-left: 5px; bottom: 2px;'><img style='position: relative; margin-top: 2px;' src='/images/save.gif'/><span style='margin-left: 4px; position: relative; top: -3px;'>Save</span></div>");
+                //okButton.jqxButton({ width: 65, height: 20 });
+                //okButton.click(function (event) {
+                //    tuneDiv.hide();
+                //});
+                //tuneDiv.append(okButton);
+                var listSource = new Array();
+                var gridColumns = $(gridName).jqxGrid('columns').records;
+                gridColumns.forEach(function (item, i) {
+                    listSource[i] = { label: item.text, value: item.datafield, checked: !item.hidden };
+                });
+                tuneDiv.jqxListBox({ source: listSource, width: 200, height: 200, checkboxes: true });
+                tuneDiv.on('checkChange', function (event) {
+                    event.stopPropagation();
+                    $(gridName).jqxGrid('beginupdate');
+                    if (event.args.checked) {
+                        $(gridName).jqxGrid('showcolumn', event.args.value);
+                    }
+                    else {
+                        $(gridName).jqxGrid('hidecolumn', event.args.value);
+                    }
+                    $(gridName).jqxGrid('endupdate');
+                });
+
+                $("body").append(tuneDiv);
+            });
+            // reload grid data.
+            reloadButton.click(function (event) {
+                $(gridName).jqxGrid('gotopage', 0);
+                $(gridName).jqxGrid({ source: cObj.GetDataAdapter() });
+            });
+            saveStateButton.click(function () {
+                // save the current state of jqxGrid.
+                $(gridName).jqxGrid('savestate');
+            });
+        }
+        //var totalcolumnrenderer = function (row, column, cellvalue) {
+        //        var cellvalue = $.jqx.dataFormat.formatnumber(cellvalue, 'c2');
+        //        return '<span style="margin: 6px 3px; font-size: 12px; float: right; font-weight: bold;">' + cellvalue + '</span>';
+        //}
+
+        var createEditor = function (row, cellvalue, editor) {
+            $('#window').jqxWindow({
+                autoOpen: false, width: 500, position: 'bottom, center', height: 400, maxWidth: 800,
+                resizable: false, isModal: true,
+                okButton: $('#ok'), cancelButton: $('#cancel'),
+                initContent: function () {
+                    $("#editor").jqxTextArea({ height: 320, width: '100%', minLength: 1 }).val(cellvalue);
+                    $('#ok').jqxButton({
+                        width: '65px',
+                        theme: 'energyblue'
+                    });
+                    $('#cancel').jqxButton({
+                        width: '65px',
+                        theme: 'energyblue'
+                    });
+                    $('#ok').focus();
+                }
+            });
+            $('#window').on('close', function (event) {
+                if (event.args.dialogResult.OK) {
+                    var rowindex = $(gridName).jqxGrid('getselectedrowindex');
+                    $(gridName).jqxGrid('beginupdate');
+
+                    var body = new Object();
+                    body.class_name = cObj.class_name;
+                    var changedObject = { id: $(gridName).jqxGrid('getrowid', rowindex) };
+                    changedObject[editedObject.DataField] = $('#editor').jqxTextArea('val');
+                    body.json_object = JSON.stringify(changedObject);
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('POST', '/Object/Update', false);
+                    xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.send(JSON.stringify(body));
+
+                    $(gridName).jqxGrid('endupdate');
+                }
+            });
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////here starts initialization of Grid////////////////////////////////////////////////////////////////////////////////
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/Object/ColumnsList?class_name=' + className, false);
+        xhr.send();
+        data = JSON.parse(xhr.responseText);
+        ColumnModels[className] = new Array();
+        var columnsInfo = JSON.parse(data.response_body);
+        if (columnsInfo && columnsInfo.length)
+            columnsInfo.forEach(function (item, i) {
+                ColumnModels[className][i] =
+                {
+                    text: item.DisplayName,
+                    datafield: item.Name,
+                    width: 80,
+                    columntype: getColumnType(item.TypeName),
+                    createeditor: createEditor,
+                    initeditor: function (row, cellvalue, editor, celltext, pressedChar) {
+                        $('#editor').jqxTextArea('val', cellvalue);
+                        $('#window').jqxWindow('open');
+                    },
+                    geteditorvalue: function (row, cellvalue, editor) {
+                        return $('#editor').jqxTextArea('val');
+                    },
+                    cellvaluechanging: function (row, column, columntype, oldvalue, newvalue) {
+                        // return the old value, if the new value is empty.
+                        //console.log(newvalue);
+                        //if (newvalue == "") return oldvalue;
+                    },
+                    cellbeginedit: function (row, datafield, columntype) {
+                        editedObject.DataField = datafield;
+                        return true;
+                    }
+                };
+            });
+
+
+        gridName = "#" + className;
+        let gridContainer = $('#grid_container');
+        gridContainer.empty();
+        $('<div>', {
+            id: className
+        }).appendTo(gridContainer);
+
+        $(gridName).jqxGrid(
+            {
+                width: getWidth('Grid'),
+                autoheight: true,
+                source: cObj.GetDataAdapter(),
+                sortable: true,
+                columnsresize: true,
+                columnsreorder: true,
+                virtualmode: true,
+                pageable: true,
+                editable: true,
+                editmode: 'dblclick',
+                rendergridrows: renderGridRows,
+                showtoolbar: true,
+                rendertoolbar: renderToolBar,
+                columns: ColumnModels[className]
+            });
+
+        $(gridName).jqxGrid({ pagermode: "simple" });
+
+        //if (!LoadedState[className]) {
+        $(gridName).jqxGrid('loadstate');
+        //    LoadedState[className] = true;
+        //}
+
+        $(gridName).on("sort", function (event) {
+            SortState[className] = event.args.sortinformation;
+            if (event.owner.dataview.pagenum == 0)
+                $(gridName).jqxGrid({ source: cObj.GetDataAdapter() });
+            else {
+                $(gridName).jqxGrid('gotoprevpage');
+                $(gridName).jqxGrid('gotonextpage');
+            }
+        });
+        $(gridName).on('rowselect', function (event) {
+            objEdit.SetObj(event.args.row);
+        });
+
+
+    }
+
     $.fn.clientObj = function (className, vueFilter) {
         var gridDiv = $("<div>");
         gridDiv.attr("id", className);
         this.append(gridDiv);
-
 
         if (cObj)
             cObj.destroy();
 
         cObj = new Vue({
             data: {
+                el: this.grid_name,
                 class_name: className,
                 filtersSource: vueFilter,
                 controllers: []
+            },
+            computed: {
+                // a computed getter
+                grid_name: function () {
+                    return '#' + this.class_name + '_';
+                }
             },
             methods: {
                 GetFilter: function () {
@@ -48,10 +286,8 @@
 
             }
         });
-        //if (!keydown_assigned) {
-        //    $(document).keydown(function (e) { cObj.ProcessKey(e.key, e.altKey, e.ctrlKey, e.shiftKey) });
-        //    keydown_assigned = true;
-        //}
+
+        InitGridFromObj(className);
 
     };
 })(jQuery);
