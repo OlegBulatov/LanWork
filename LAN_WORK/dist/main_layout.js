f0cb0bc88689004996ac0c8685a7f7c2b757b42b
Oleg Bulatov <obulatov@mail.ru>
2020-04-18 18:09:52 +0500

    ver 1.0 of this branch

--- LAN_WORK/dist/main_layout.js
+++ LAN_WORK/dist/main_layout.js
@@ -1,4 +1,5 @@
-    var currentModuleName = 'Help';
+﻿var currentModuleName = 'Help';
+var clickAfterSelect = false;
     var layout =
                 [
                     {
@@ -18,48 +19,113 @@
                                                 contentContainer: 'ModulesTree',
                                                 initContent: function () {
                                                     var $tree = $('#modulesTree');
-
-                                                    $tree.jstree({
-                                                        'core': {
-                                                            "check_callback": true,
-                                                            'data': {
-                                                                'multiple': false,
-                                                                "url": "/demotree.json",
-                                                                "dataType": "json",
-                                                                "data": function (node) {
-                                                                    return { "id": node.id };
-                                                                }
-                                                            }
+                                                    var data = [
+                                                        {
+                                                            "id": "1", "text": "Администрирование"
                                                         },
-                                                        'plugins': ["state", "contextmenu"]
-                                                    });
+                                                        { "id": "3", "parentid": "1", "text": "Объекты", "value": "OBJECT_TYPE" },
+                                                        { "id": "4", "parentid": "1", "text": "Модули", "value": "MODULE" },
+                                                        { "id": "5", "parentid": "1", "text": "Формы", "value": "FORM" },
+                                                        {
+                                                            "id": "2", "text": "Модули"
+                                                        },
+                                                        { "id": "6", "parentid": "2", "text": "Серверы", "value": "LNT_SERVER" },
+                                                        { "id": "7", "parentid": "2", "text": "Заявки JIRA", "value": "JIRA_ISSUE" }
 
-                                                    $tree.on("select_node.jstree", function (e, data) {
+                                                    ];
+                                                    var source =
+                                                    {
+                                                        datatype: "json",
+                                                        datafields: [
+                                                            { name: 'id' },
+                                                            { name: 'parentid' },
+                                                            { name: 'text' },
+                                                            { name: 'value' }
+                                                        ],
+                                                        id: 'id',
+                                                        localdata: data
+                                                    };
+                                                    // create data adapter.
+                                                    var dataAdapter = new $.jqx.dataAdapter(source);
+                                                    // perform Data Binding.
+                                                    dataAdapter.dataBind();
+                                                    // get the tree items. The first parameter is the item's id. The second parameter is the parent item's id. The 'items' parameter represents 
+                                                    // the sub items collection name. Each jqxTree item has a 'label' property, but in the JSON data, we have a 'text' field. The last parameter 
+                                                    // specifies the mapping between the 'text' and 'label' fields.  
+                                                    var records = dataAdapter.getRecordsHierarchy('id', 'parentid', 'items', [{ name: 'text', map: 'label' }]);
+                                                    $tree.jqxTree({ source: records, width: '300px' });
+                                                    $tree.on('select', function (event) {
+                                                        var args = event.args;
+                                                        var item = $tree.jqxTree('getItem', args.element);
                                                         var helpText = document.getElementById('helpText');
-                                                        if (data.selected.length) {
-                                                            var selected = data.instance.get_node(data.selected[0]);
-                                                            currentModuleName = selected.data;
-                                                        }
+                                                        currentModuleName = item.value;
                                                         var moduleOpened = showPanel(currentModuleName);
                                                         if (!moduleOpened) {
-                                                            helpText.innerText = 'For opening of module ' + currentModuleName + ' use mouse double click';
+                                                            if (currentModuleName) {
+                                                                helpText.innerText = 'Для открытия модуля ' + currentModuleName + ' еще раз нажмите кнопку мыши';
+                                                                clickAfterSelect = true;
+                                                            }
+                                                            else
+                                                                helpText.innerText = '';
                                                             showPanel('Help');
                                                         }
                                                     });
-                                                    $tree.on("dblclick.jstree", function (e, data) {
-                                                        //if (data.selected.length) {
-                                                        //    var selected = data.instance.get_node(data.selected[0]);
-                                                        //    var moduleName = selected.data;
-                                                        //    //if (moduleName != currentModuleName) {
-                                                        //        //LoadFancyGrid(selected.data);
-                                                        helpText.innerText = 'You can show already opened module with mouse click';
-                                                        if (!showPanel(currentModuleName)) 
-                                                              //      currentModuleName = moduleName;
-                                                                //else
-                                                            initBrowser(currentModuleName);
-                                                        //    //}
-                                                        //}
+                                                    $tree.on('itemClick', function (event) {
+                                                        if (!clickAfterSelect) {
+                                                            var args = event.args;
+                                                            var item = $tree.jqxTree('getItem', args.element);
+                                                            if (item.value) {
+                                                                helpText.innerText = 'Уже открытый модуль можно открыть повторно, выбрав мышью в дереве модулей';
+                                                                if (!showPanel(currentModuleName)) 
+                                                                    initBrowser(currentModuleName);
+                                                            }
+                                                        }
+                                                        clickAfterSelect = false;
+                                                        //alert('itemClick');
                                                     });
+                                                    //var $tree = $('#modulesTree');
+
+                                                    //$tree.jstree({
+                                                    //    'core': {
+                                                    //        "check_callback": true,
+                                                    //        'data': {
+                                                    //            'multiple': false,
+                                                    //            "url": "/demotree.json",
+                                                    //            "dataType": "json",
+                                                    //            "data": function (node) {
+                                                    //                return { "id": node.id };
+                                                    //            }
+                                                    //        }
+                                                    //    },
+                                                    //    'plugins': ["state", "contextmenu"]
+                                                    //});
+
+                                                    //$tree.on("select_node.jstree", function (e, data) {
+                                                    //    var helpText = document.getElementById('helpText');
+                                                    //    if (data.selected.length) {
+                                                    //        var selected = data.instance.get_node(data.selected[0]);
+                                                    //        currentModuleName = selected.data;
+                                                    //    }
+                                                    //    var moduleOpened = showPanel(currentModuleName);
+                                                    //    if (!moduleOpened) {
+                                                    //        helpText.innerText = 'For opening of module ' + currentModuleName + ' use mouse double click';
+                                                    //        showPanel('Help');
+                                                    //    }
+                                                    //});
+                                                    //$tree.on("dblclick.jstree", function (e, data) {
+                                                    //    //if (data.selected.length) {
+                                                    //    //    var selected = data.instance.get_node(data.selected[0]);
+                                                    //    //    var moduleName = selected.data;
+                                                    //    //    //if (moduleName != currentModuleName) {
+                                                    //    //        //LoadFancyGrid(selected.data);
+                                                    //    helpText.innerText = 'You can show already opened module with mouse click';
+                                                    //    if (!showPanel(currentModuleName)) 
+                                                    //          //      currentModuleName = moduleName;
+                                                    //            //else
+                                                    //        initBrowser(currentModuleName);
+                                                    //    //    //}
+                                                    //    //}
+                                                    //});
 
                                                 }
                                             }
