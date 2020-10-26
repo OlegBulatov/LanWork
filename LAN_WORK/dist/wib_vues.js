10907c33939b8325de6f20c8259186b65abf4714
Oleg Bulatov <obulatov@mail.ru>
2020-10-26 14:23:55 +0300

    draggable buttons

--- LAN_WORK/dist/wib_vues.js
+++ LAN_WORK/dist/wib_vues.js
@@ -2,26 +2,28 @@ Vue.component('wib_button', {
 	computed: {
 		displayStyle: function () {
 			return {
-				  position: 'absolute',
-				  zIndex: 200,
-				  background: 'blue',
-				  color: 'red',
-				  left: this.left + 'px',
-				  top: this.top + 'px'
+				position: 'absolute',
+				zIndex: 150,
+				background: 'cyan',
+				color: 'red',
+				left: this.left + 'px',
+				top: this.top + 'px',
+				border: this.draggable? '2px solid red' : 'none'
 				}
 		},
 	},
-	props: ['caption','top','left'],
-	template: '<button onclick="alert(\'!\')" v-bind:style="displayStyle">{{this.caption}}</button>'
+	props: ['caption','top','left','draggable'],
+	template: '<button onclick="alert(\'!\')" v-bind:style="displayStyle" v-bind:draggable="draggable">{{this.caption}}</button>'
 });
 
 Vue.component('wib_text', {
 	computed: {
 		displayStyle: function () {
 return {
+      border: '2px dotted black',
       position: 'absolute',
 	  zIndex: 200,
-      background: 'cyan',
+      background: '#FFA0FF',
       color: 'black',
 	  width: this.width + 'px',
 	  height: this.height + 'px',
@@ -55,8 +57,9 @@ return {
 		wib_buttonComponents.attr("v-for", "v_wib_button in buttons");
 		wib_buttonComponents.attr("v-bind:Id", "v_wib_button.Id");
 		wib_buttonComponents.attr("v-bind:Caption", "v_wib_button.Caption");
-		wib_buttonComponents.attr("v-bind:targetNodeId", "v_wib_button.targetNodeId");
+		wib_buttonComponents.attr("v-bind:draggable", "v_wib_button.draggable");
 		wib_buttonComponents.attr("v-bind:Top", "v_wib_button.Top");
+		wib_buttonComponents.attr("v-bind:targetNodeId", "v_wib_button.targetNodeId");
 		wib_buttonComponents.attr("v-bind:Left", "v_wib_button.Left");
 		this.append(wib_buttonComponents);
 
@@ -64,7 +67,7 @@ return {
 			el: this.selector, 
 			data: {
 				pictureUrl: "/user_resources/lanitadmin/5f7293b9-f9b0-4804-81b9-92a5e9c50507.png",
-				buttons: [{Caption: "Test", Left: 100, Top: 200}],
+				buttons: [],
 				texts: [{text: "Test text just for test", left: 300, top: 200, width: 100, height: 100}]
 			},
 			computed: {
@@ -82,6 +85,13 @@ return {
 				},
 				SetButtons(btns) {
 					this.buttons = btns;
+				},
+				SetButtonsDraggable(drg) {
+					this.buttons.forEach(function(item){
+						item.draggable = drg;
+						//item.Caption += "-"; 
+					});
+					this.$forceUpdate();
 				}
 			}
 		});
