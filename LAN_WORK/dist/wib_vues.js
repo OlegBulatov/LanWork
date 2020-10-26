248ba07c7e0d047bf214d0a293daf07d00230433
Oleg Bulatov <obulatov@mail.ru>
2020-10-26 18:27:31 +0300

    drag and drop buttons and textst

--- LAN_WORK/dist/wib_vues.js
+++ LAN_WORK/dist/wib_vues.js
@@ -1,22 +1,38 @@
 Vue.component('wib_button', {
+	mounted: function () {
+		$('#' + this.id).draggable({
+			stop: function (ev) {
+				//if (this.__vue__ && this.__vue__.app_index >= 0) {
+				//	appDiagram.ctrls[this.__vue__.app_index].left = $('#' + this.id).position().left;
+				//	appDiagram.ctrls[this.__vue__.app_index].top = $('#' + this.id).position().top;
+				//}
+			}
+		});
+	},
 	computed: {
 		displayStyle: function () {
 			return {
 				position: 'absolute',
 				zIndex: 150,
-				background: 'cyan',
+				background: 'black',
 				color: 'red',
-				left: this.left + 'px',
-				top: this.top + 'px',
+				//opacity: 0.7,
+				left: (this.left - 5) + 'px',
+				top: (this.top - 5) + 'px',
+				width: '5px',
+				height: '5px',
 				border: this.draggable? '2px solid red' : 'none'
 				}
 		},
 	},
-	props: ['caption','top','left','draggable'],
-	template: '<button onclick="alert(\'!\')" v-bind:style="displayStyle" v-bind:draggable="draggable">{{this.caption}}</button>'
+	props: ['id', 'caption', 'top', 'left', 'draggable'],
+	template: '<div v-bind:id="id" v-bind:style="displayStyle"><button style="position:absolute; left:5px; top:5px; height: 25px" onclick="alert(\'!\')" >{{this.caption}}</button></div>'
 });
 
 Vue.component('wib_text', {
+	mounted: function () {
+		$('#' + this.id).draggable();
+	},
 	computed: {
 		displayStyle: function () {
 return {
@@ -32,13 +48,14 @@ return {
     }
 		},
 	},
-	props: ['width','height','text','top','left'],
-	template: '<div v-bind:style="displayStyle">{{this.text}}</div>'
+	props: ['id','width','height','text','top','left'],
+	template: '<div v-bind:id="id" v-bind:style="displayStyle">{{this.text}}</div>'
 });
 
 (function ($) {
 	$.fn.wibPage = function () {
 //initialization code
+
 		var pictureContainer = $("<div>");
 		pictureContainer.attr("v-bind:style", "displayStyle");
 		this.append(pictureContainer);
@@ -46,6 +63,7 @@ return {
 
 		var wib_textComponents = $("<wib_text>");
 		wib_textComponents.attr("v-for", "v_wib_text in texts");
+		wib_textComponents.attr("v-bind:id", "v_wib_text.id");
 		wib_textComponents.attr("v-bind:width", "v_wib_text.width");
 		wib_textComponents.attr("v-bind:height", "v_wib_text.height");
 		wib_textComponents.attr("v-bind:text", "v_wib_text.text");
@@ -57,18 +75,24 @@ return {
 		wib_buttonComponents.attr("v-for", "v_wib_button in buttons");
 		wib_buttonComponents.attr("v-bind:Id", "v_wib_button.Id");
 		wib_buttonComponents.attr("v-bind:Caption", "v_wib_button.Caption");
-		wib_buttonComponents.attr("v-bind:draggable", "v_wib_button.draggable");
+		//wib_buttonComponents.attr("v-bind:draggable", "v_wib_button.draggable");
 		wib_buttonComponents.attr("v-bind:Top", "v_wib_button.Top");
 		wib_buttonComponents.attr("v-bind:targetNodeId", "v_wib_button.targetNodeId");
 		wib_buttonComponents.attr("v-bind:Left", "v_wib_button.Left");
 		this.append(wib_buttonComponents);
+
+		//var dropContainer = $("<div>");
+		//dropContainer.attr("ondrop", "return DragDrop(event);");
+		//dropContainer.attr("id", "dropContainer");
+		//dropContainer.attr("style", "position: absolute; left: 0px; top: 0px; width: 100%; height: 100%;");
+		//this.append(dropContainer);
 
 		return new Vue({
 			el: this.selector, 
 			data: {
 				pictureUrl: "/user_resources/lanitadmin/5f7293b9-f9b0-4804-81b9-92a5e9c50507.png",
 				buttons: [],
-				texts: [{text: "Test text just for test", left: 300, top: 200, width: 100, height: 100}]
+				texts: [{id: "text1", text: "Test text just for test", left: 300, top: 200, width: 100, height: 100}]
 			},
 			computed: {
 				displayStyle: function () {
@@ -92,9 +116,29 @@ return {
 						//item.Caption += "-"; 
 					});
 					this.$forceUpdate();
-				}
+				},
+				DragOver(ev) {
+					ev.preventDefault();
+				},
+				//DragDrop(ev) {
+				//	alert('drop!!!');
+				//	//let N = app.entities.length;
+				//	//app.entities.push({ displayed_name: 'new div', id: 'n' + (N + 1), app_index: N, left: ev.offsetX - 10, top: ev.offsetY - 10, width: 100, height: 25, is_selected: false, is_visible: true });
+				//	//app.relations.push({ entity_a: app.entities[N - 1], entity_b: app.entities[N] });
+				//	ev.stopPropagation();
+				//	return false;
+				//}
 			}
 		});
 	}
 })(jQuery);
 
+
+//function DragDrop(ev) {
+//	alert('!!!');
+//	//let N = app.entities.length;
+//	//app.entities.push({ displayed_name: 'new div', id: 'n' + (N + 1), app_index: N, left: ev.offsetX - 10, top: ev.offsetY - 10, width: 100, height: 25, is_selected: false, is_visible: true });
+//	//app.relations.push({ entity_a: app.entities[N - 1], entity_b: app.entities[N] });
+//	ev.stopPropagation();
+//	return false;
+//}
