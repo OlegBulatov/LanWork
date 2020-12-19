1cc0e3bc3ba2104cc7a9807b501c9ee8f85e43c2
Oleg Bulatov <obulatov@mail.ru>
2020-12-19 08:43:58 +0300

    + to previous

---
+++ Bloomberg/DuplexClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace DuplexClient
+{
+    public class CallbackHandler : CalculatorService.ICalculatorServiceCallback
+    {
+        public void Result(double result)
+        {
+            Console.WriteLine("Result({0})", result);
+        }
+
+        public void Equation(string equation)
+        {
+            Console.WriteLine("Equation: {0}", equation);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Construct InstanceContext to handle messages on callback interface.
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+
+            // Create a client.
+            var client = new CalculatorService.CalculatorServiceClient(instanceContext);
+
+            Console.WriteLine("Press <ENTER> to terminate client once the output is displayed.");
+            Console.WriteLine();
+
+            // Call the AddTo service operation.
+            double value = 100.00D;
+            client.AddTo(value);
+
+            // Call the SubtractFrom service operation.
+            value = 50.00D;
+            client.SubtractFrom(value);
+
+            // Call the MultiplyBy service operation.
+            value = 17.65D;
+            client.MultiplyBy(value);
+
+            // Call the DivideBy service operation.
+            value = 2.00D;
+            client.DivideBy(value);
+
+            // Complete equation.
+            client.Clear();
+
+            Console.ReadLine();
+
+            //Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+        }
+    }
+}
---
+++ Bloomberg/DuplexServer/Service1.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DuplexServer
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class CalculatorService : ICalculatorDuplex
+    {
+        double result = 0.0D;
+        string equation;
+
+        public CalculatorService()
+        {
+            equation = result.ToString();
+        }
+
+        public void Clear()
+        {
+            Callback.Equation($"{equation} = {result}");
+            equation = result.ToString();
+        }
+
+        public void AddTo(double n)
+        {
+            result += n;
+            equation += $" + {n}";
+            Callback.Result(result);
+        }
+
+        public void SubtractFrom(double n)
+        {
+            result -= n;
+            equation += $" - {n}";
+            Callback.Result(result);
+        }
+
+        public void MultiplyBy(double n)
+        {
+            result *= n;
+            equation = "(" + equation + ")";
+            equation += $" x {n}";
+            Callback.Result(result);
+        }
+
+        public void DivideBy(double n)
+        {
+            result /= n;
+            equation = "(" + equation + ")";
+            equation += $" : {n}";
+            Callback.Result(result);
+        }
+
+        ICalculatorDuplexCallback Callback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<ICalculatorDuplexCallback>();
+            }
+        }
+    }
+}
